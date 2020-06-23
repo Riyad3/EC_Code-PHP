8e5558dd5a52266b911b534dc3dd434fa2eb9485
Unknown <riyad.achour@edu.itescia.fr>
2020-06-23 12:10:21 +0200

    function registrate create and done

--- ec-code-2020-codflix-php-master/controller/signupController.php
+++ ec-code-2020-codflix-php-master/controller/signupController.php
@@ -22,3 +22,47 @@ function signupPage() {
 /***************************
 * ----- SIGNUP FUNCTION -----
 ***************************/
+function registration() {
+
+  // On verifie d'abord qu'on reçoit des données via le formulaire 
+  if(!empty($_POST)){
+    extract($_POST);
+    $valid = true;
+
+  // On attribut les valeur recuperer dans le form
+    if(isset($_POST['Valider'])){
+      $mail = htmlentities(strtolower($email));
+      $password = htmlentities(trim($password));
+      $conf_password = htmlentities(trim($password_confirm));
+
+      if(empty($mail)){
+        $valid = false;
+        $er_mail = "Le mail ne peut pas être vide";
+      }
+      elseif(!preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $mail)){
+        $valid = false;
+        $er_mail = "Le mail n'est pas valide";
+      }
+    }
+    if(empty($password)) {
+      $valid = false;
+      $er_mdp = "Le mot de passe ne peut pas être vide";
+       }
+       elseif($password != $conf_password)
+       {
+         $valid = false;
+         $er_mdp = "La confirmation du mot de passe ne correspond pas";
+    }
+    if($valid){
+      $password = hash('sha256', $password);
+      $new_user = new User();
+      $new_user->setEmail($mail);
+      $new_user->setPassword($password);
+      $new_user->createUser();
+  
+      }
+    }
+  }
+
+
+
